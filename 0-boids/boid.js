e6d002b4daf73886d063b45a19cbdf155aedb034
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-12-02 19:15:51 -0600

    Added cohesion to the alignment on flocking

--- 0-boids/boid.js
+++ 0-boids/boid.js
@@ -1,6 +1,8 @@
 const conformity = 0.2;
 const perception = 50;
 const speedy = 4;
+var applyCohesion = true;
+var applyAlignment = true;
 
 
 class Boid{
@@ -61,10 +63,50 @@ class Boid{
     return steer;
   }
 
+// steer towards the average of nearby boids average POSITION
+  cohesion(boids){
+    let perceptionRadius = perception;
+    let nearby = 0;
+    let steer = createVector();
+
+    // get average direction all boids within this boids perception( the steering velocity)
+    for (let other of boids){
+      let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+      if (other != this && d<perceptionRadius){
+        steer.add(other.position);
+        nearby++;
+      }
+    }
+
+    if(nearby > 0){
+      steer.div(nearby);
+      // apply stearing formula to guide boid into flock
+      steer.sub(this.position)
+      steer.setMag(this.maxSpeed)
+      steer.sub(this.velocity)
+      // limit the ability for a boid to instantly conform to the flock
+      steer.limit(this.maxForce);
+    }
+    return steer;
+  }
+
+
   flock(boids){
     this.edges();
+
+    // start from 0 force, instead of adding onto what already exists
+    this.acceleration.set(0, 0);
+
+    // get nearby forces
+    let cohesion = this.cohesion(boids)
     let alignment = this.align(boids);
-    this.acceleration = alignment;
+
+    // force accumulation 
+    this.acceleration.add(cohesion);
+    this.acceleration.add(alignment);
+    
+    // actually limit to maximum speed
+    this.velocity.limit(this.maxSpeed)
   }
 
   show(){
