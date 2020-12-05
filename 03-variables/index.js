62010627bd172bf96e48dc74934c6a4e63cd0b5e
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-12-05 12:17:04 -0600

    day 3 working - adjusting a frosted glass dispay with CSS Variables

---
+++ 03-variables/index.js
@@ -0,0 +1,9 @@
+const inputs = document.querySelectorAll("input");
+
+function handleUpdate(){
+  const suffex=this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value+suffex)
+}
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
