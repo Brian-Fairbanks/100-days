dee8a7c716b1a8a17a07bb5ce73331fdebbe7b65
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-12-05 14:29:12 -0600

    Added background image changing, along with title and credit

--- 03-variables/index.js
+++ 03-variables/index.js
@@ -1,9 +1,18 @@
 const inputs = document.querySelectorAll("input");
+const imgs = document.querySelectorAll("button");
+
+const title = $("#title");
+const credit = $("#credit");
 
 function handleUpdate(){
   const suffex=this.dataset.sizing || '';
   document.documentElement.style.setProperty(`--${this.name}`, this.value+suffex)
+
+  if(this.dataset.title){ console.log("change the title!"); title.text(this.dataset.title);}
+  if(this.dataset.credit){credit.text(this.dataset.credit);}
 }
 
 inputs.forEach(input => input.addEventListener("change", handleUpdate));
 inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
+
+imgs.forEach(input => input.addEventListener("click", handleUpdate));
