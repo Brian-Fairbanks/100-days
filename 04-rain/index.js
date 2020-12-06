53493fe34034735fe4cf559ea64cb45ef286eada
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-12-06 15:31:29 -0600

    Snow catches on mouse

--- 04-rain/index.js
+++ 04-rain/index.js
@@ -1,7 +1,13 @@
 
-let drops = [];
+let flakes = [];
 // let alignSlider, cohesionSlider, seperationSlider;
 let raining;
+let mousePos
+
+function setMouse(e){
+  mousePos = createVector(e.offsetX,e.offsetY);
+  console.log(mousePos)
+}
 
 function setup(){
   createCanvas(1024, 768);
@@ -13,32 +19,58 @@ function setup(){
   // createDiv("Seperation");
   // seperationSlider = createSlider(0,5,1, 0.1)
 
-  raining = setInterval(function (){drops.push(new Drop)} ,10);
+  raining = setInterval(function (){flakes.push(new Drop)} ,10);
+  canvas.addEventListener("mousemove", setMouse);
+
 
 }
 
 function draw(){
   background(51);
 
-  for (let drop of drops){
+  for (let drop of flakes){
     drop.update();
   }
 
   // filter out passed drops
-  drops = drops.filter(drop => (drop.position.y < height && drop.position.x > 0 && drop.position.x < width))
+  flakes = flakes.filter(drop => (drop.position.y < height && drop.position.x > 0 && drop.position.x < width))
 }
+
+
+
+
+
 
 
 class Drop{
   constructor(){
     this.position = createVector(random(width), 0);
-    this.velocity = createVector(random(width), abs(random(height)))
+    this.velocity = createVector(.5*width - random(width), abs(random(height)))
     this.velocity.limit(1);
     this.size = random(3);
+    this.falling = true;
   }
 
   update(){
-    this.position.add(this.velocity);
+    if (this.falling){
+      this.position.add(this.velocity);
+    }
+
+    let d;
+    
+    if (mousePos){
+      d = dist(this.position.x, this.position.y, mousePos.x, mousePos.y);
+    }
+    // let d = 100;
+    if(d < 20){
+      this.position
+      this.falling=false;
+      this.velocity = createVector(0, 3);
+    }
+    else{
+      this.falling=true;
+    }
+
     this.show();
   }
 
