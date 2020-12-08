bd03056fcec69698b00f82249746055f898a810f
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-12-08 12:21:33 -0600

    transition speeds/delays are working now

--- 06-flexPanel/index.js
+++ 06-flexPanel/index.js
@@ -1,10 +1,19 @@
 const panels = document.querySelectorAll(".panel");
 console.log(panels);
 
+function toggleActive(e){
+  panels.forEach(each => {each.classList.remove("active")})
+  this.classList.add("active");
+}
+
+function toggleOpen(e){
+  if(e.propertyName.includes('flex')){
+    this.classList.toggle("open");
+  }
+}
+
 panels.forEach(function(panel){
   console.log("Adding listener")
-  panel.addEventListener("mouseover", e => {
-    panels.forEach(each => {each.classList.remove("active")})
-    panel.classList.add("active");
-  })
+  panel.addEventListener("click", toggleActive)
+  panel.addEventListener("transitionend", toggleOpen)
 })
