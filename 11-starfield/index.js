b74c2487afd750969bf0b966c5c526e86dfbd5d5
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-12-13 21:26:08 -0600

    added brief instructions at start

--- 11-starfield/index.js
+++ 11-starfield/index.js
@@ -1,8 +1,15 @@
 const stars = [];
 var speed = 4;
+var timer = 0;
+
+// interactive range
+var ir = .3;
+
+let instruction = "UP/DOWN to control speed \n Mouse to steer";
+
 
 function setup(){
-  createCanvas(1024, 768);
+  createCanvas(windowWidth, windowHeight);
 
   for (let i=0; i<800; i++){
     stars.push(new Star());
@@ -12,14 +19,28 @@ function setup(){
 
 function draw(){
   background(0);
+  
+  if(timer < 300){timer+=1;}
+
+  if(timer < 150 || (timer%20<=10 && timer<300)){
+    fill(255);
+    stroke(255);
+    textSize(width/20);
+    textAlign(CENTER, CENTER);
+    text(instruction, 0, 0, width, height); // Text wraps within text box\
+  }
 
   // reset to center
-  translate(mouseX, mouseY);
+  translate(map(mouseX, 0, width, 0.5 + ir, 0.5 - ir) * width, map(mouseY, 0, height, 0.5 + ir, 0.5 - ir) * height);
 
   for (let star of stars){
     star.update();
     star.show();
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 
