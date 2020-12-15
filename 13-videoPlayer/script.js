49edb817f87480aa5112525bfc3b261396dc0ea3
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-12-15 11:25:58 -0600

    now changes the play/pause indicators

--- 13-videoPlayer/script.js
+++ 13-videoPlayer/script.js
@@ -26,24 +26,35 @@ function handleKeyboard(e){
 }
 
 function togglePlay(){
-  if (video.paused){
-    pauseIndicator.classList.add("showFade");
-    video.play();
-  }
-  else{
-    video.pause()
-    pauseIndicator.classList.add("showFade");
-  };
+  const action = video.paused?'play':'pause';
+  video[action]();
+}
+
+function updatePlayIndicator(){
+  const icon = this.paused?'❚❚':'▶';
+  playPause.textContent=icon;
+  pauseIndicator.textContent=icon;
+
+  pauseIndicator.classList.add("showFade");
 }
 
 /*
 |  Listeners
 =========================================================*/
+// play pause actions
 playPause.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
 
+// update buttons
+video.addEventListener('play', updatePlayIndicator);
+video.addEventListener('pause', updatePlayIndicator)
+
+
+
 document.addEventListener('keydown', e => {handleKeyboard(e)});
 
+
+// remove animation after it runs 
 pauseIndicator.addEventListener('animationend', e => {
   e.target.classList.remove("showFade")
 })
