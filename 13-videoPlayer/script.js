303e856c0371682d9ff67fe999cd695232e06168
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-12-15 09:54:47 -0600

    added link to main page - set up filestructure - set up styles

---
+++ 13-videoPlayer/script.js
@@ -0,0 +1,23 @@
+/*
+|  Variables
+=========================================================*/
+
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress_filled")
+
+const playPause = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelector(".player__slider")
+
+
+/*
+|  Functions
+=========================================================*/
+
+
+/*
+|  Listeners
+=========================================================*/
